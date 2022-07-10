1c924b2e17befe716e66db8f60ec3c297e311c36
Marco Estors <marco_trabajo@yahoo.com>
2022-07-10 22:38:22 +0200

    ecommerce html css javascript done, only needs adjusting hero text

--- js/01-header.js
+++ js/01-header.js
@@ -1,1 +1,35 @@
-alert("hello world... this is js/01-header.js");
+
+// Show / hide mobile menu 
+const mobileMenuIcon = document.querySelector(".header-mobile-menu-icon");
+const mobileMenu = document.querySelector(".header__bottom__nav");
+const hero = document.querySelector(".hero");
+
+function showHideMobileMenu() {
+    mobileMenu.classList.toggle("showMobileMenu");
+    headerSearchForm.classList.remove("show-header__search__form");
+}
+
+mobileMenuIcon.addEventListener("click", showHideMobileMenu);
+
+
+// Show / hide header search bar 
+const headerSearchIcon = document.querySelector(".header__search__icon");
+const headerSearchForm = document.querySelector(".header__search__form");
+
+function showHeaderSearch() {
+    headerSearchForm.classList.toggle("show-header__search__form");
+    mobileMenu.classList.remove("showMobileMenu");
+}
+
+headerSearchIcon.addEventListener("click", showHeaderSearch);
+
+
+// hide mobile menu and header search bar --> on scroll and on click hero section
+function hideMobileMenu_hideHeaderSearchBar() {
+    headerSearchForm.classList.remove("show-header__search__form");
+    mobileMenu.classList.remove("showMobileMenu");
+}
+
+hero.addEventListener("click", hideMobileMenu_hideHeaderSearchBar);
+window.addEventListener("scroll", hideMobileMenu_hideHeaderSearchBar);
+
--- js/03-swiper-slider-touch.js
+++ js/03-swiper-slider-touch.js
@@ -1,24 +1,47 @@
+
+
+
+
+
+
 const swiper = new Swiper('.swiper', {
 // Default parameters
 slidesPerView: 4,
-spaceBetween: 10,
+spaceBetween: 40,
 // Responsive breakpoints
 breakpoints: {
+    // when window width is >= 120px
+    0: {
+    slidesPerView: 2,
+    spaceBetween: 10
+    },
     // when window width is >= 320px
     320: {
     slidesPerView: 2,
-    spaceBetween: 20
+    spaceBetween: 10
     },
     // when window width is >= 480px
     480: {
-    slidesPerView: 3,
-    spaceBetween: 30
+    slidesPerView: 2,
+    spaceBetween: 20
     },
     // when window width is >= 640px
     640: {
+    slidesPerView: 3,
+    spaceBetween: 20
+    },
+
+    822: {
+    slidesPerView: 3,
+    spaceBetween: 20
+    },
+
+    823: {
     slidesPerView: 4,
-    spaceBetween: 40
-    }
+    spaceBetween: 20
+    },
+
+
 },
 // If we need pagination
 pagination: {
